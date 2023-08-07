48747f3082370b86e6bbdb407849c11fa1384cd4
Timilehin Bello <emmatimmybello@gmail.com>
2023-08-07 12:35:59 +0100

    add console log

--- controllers/postController.js
+++ controllers/postController.js
@@ -21,6 +21,7 @@ exports.createPost = async (req, res) => {
     fs.renameSync(path, newPath);
 
     const { token } = req.cookies;
+    console.log(token);
     jwt.verify(token, process.env.JWT_SECRET, {}, async (err, info) => {
       if (err) throw err;
       const postDoc = await Post.create({
